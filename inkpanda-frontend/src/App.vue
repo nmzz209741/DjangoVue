9fe63e0f17b21e72e5786f7bfbd61a760cac7b15
Shreeti Shrestha <071bct541@pcampus.edu.np>
2020-08-23 14:34:36 +0545

    Frontend complete

---
+++ inkpanda-frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h2><router-link to="/index">Poems</router-link></h2>
+        <router-link to="/create" class="btn btn-sm btn-primary">Add Poem</router-link>
+
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ inkpanda-frontend/src/components/Create.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="pt-5">
+        <form @submit.prevent="create" method="post">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="title"
+                    v-model="poem.title"
+                    v-validate="'required'"
+                    name="title"
+                    placeholder="Enter title"
+                    :class="{'is-invalid': errors.has('poem.title') && submitted}">
+                <div class="invalid-feedback">
+                    Please provide a valid title.
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="body">body</label>
+                <textarea
+                    name="body"
+                    class="form-control"
+                    id="body"
+                    v-validate="'required'"
+                    v-model="poem.body"
+                    cols="30"
+                    rows="2"
+                    :class="{'is-invalid': errors.has('poem.body') && submitted}"></textarea>
+                <div class="invalid-feedback">
+                    Please provide a valid body.
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            poem: {
+                title: '',
+                body: '',
+            },
+            submitted: false
+        }
+    },
+    methods: {
+        create: function () {
+            this.$validator.validate().then(result => {
+                this.submitted = true;
+                if (!result) {
+                    return;
+                }
+                axios
+                    .post('http://127.0.0.1:8000/api/poem/',
+                        this.poem
+                    )
+                    .then(() => {
+                        this.$router.push('/');
+                    })
+            });
+        }
+    },
+}
+</script>
